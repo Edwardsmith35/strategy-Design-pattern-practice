5cec7e1a549632b6329488401f86ff9eb15bc07a
Edwardsmith35 <alialalel2@gmail.com>
2025-06-26 12:25:21 +0300

    fixed bugs

--- context.py
+++ context.py
@@ -1,17 +1,21 @@
 from concrete_strategies import NoDiscount
 from strategy_interface import DiscountStrategy
 
-class Order():
+
+class Order:
     """
     The Context defines the interface of interest to clients.
     It maintains a reference to one of the Strategy objects.
     """
+
     def __init__(self, total: float, strategy: DiscountStrategy = None):
         self._total = total
         self._strategy = strategy if strategy else NoDiscount()
-        print(f"Order Created with total: ${self.total:.2f} and Strategy: {self._strategy.__class__.__name__}")
-    
-    @property # This defines 'total' as a read-only property
+        print(
+            f"Order Created with total: ${self.total:.2f} and Strategy: {self._strategy.__class__.__name__}"
+        )
+
+    @property  # The @property decorator is a Pythonic way to manage attribute access. It allows you to define methods that are accessed like attributes (i.e., without parentheses ()), giving you more control over getting, setting, and deleting an attribute's value.
     def total(self):
         return self._total
 
@@ -19,6 +23,6 @@ class Order():
         """Allows replacing the strategy at runtime."""
         print(f"Changing discount strategy to: {strategy.__class__.__name__}")
         self._strategy = strategy
-    
+
     def get_final_price(self):
         return self._strategy.apply_discount(self.total)
